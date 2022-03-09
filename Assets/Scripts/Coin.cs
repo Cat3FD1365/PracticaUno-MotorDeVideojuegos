75526126ef339ad17ebd7e046e71b1f5c3c8dd18
Cat3FD1365 <82287113+Cat3FD1365@users.noreply.github.com>
2022-03-08 19:08:50 -0600

    Versión 2
    Se mejoró el mapa, se incluyó límites (Colliders) y se agregó occlusion

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
     // Variables
     public static int countCoins = 0;
+    //public Text GameOverText;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public float maxTime = 60.0f;
     private float countDown = 0.0f;
+    public Text TimerText;
+    private float tiempo;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +27,12 @@ public class Timer : MonoBehaviour
             string currentSceneName = SceneManager.GetActiveScene().name;
             SceneManager.LoadScene(currentSceneName);
         }
+        writeTimer(tiempo);
+    }
 
+    private void writeTimer(float tiempo)
+    {
+        tiempo = countDown;
+        TimerText.text = "Tiempo restante: " + tiempo.ToString() + " Seg.";
     }
 }
